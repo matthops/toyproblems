02977e1626778116bc5e26f48b40f8499d920cb5
Matt Hopkins <matthops@gmail.com>
2019-11-18 11:47:41 -0800

    reverse integer

--- OneHundredToyProblems.js
+++ OneHundredToyProblems.js
@@ -20,3 +20,34 @@ var twoSum = function(nums, target) {
     }
   }
 };
+
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+// Example 1:
+
+// Input: 123
+// Output: 321
+// Example 2:
+
+// Input: -123
+// Output: -321
+// Example 3:
+
+// Input: 120
+// Output: 21
+// Note:
+// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+var reverse = function(x) {
+  let numArr = [...(x + '')];
+  let reverseArr = [];
+  for (var i = numArr.length - 1; i >= 0; i--) {
+    reverseArr.push(parseInt(numArr[i]));
+  }
+  const finalInt = parseInt(reverseArr.join(''));
+
+  if (finalInt > 2147483648) {
+    return 0;
+  }
+  return finalInt * Math.sign(x);
+};
